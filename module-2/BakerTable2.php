e91e7452e669e8ce8f85af919b820570b008bce8
ZaccBaker <zac.baker232@gmail.com>
2026-04-01 14:01:33 -0500

    assignment(2.2): 4/1/26

---
+++ module-2/BakerTable2.php
@@ -0,0 +1,46 @@
+<!-- Zac Baker -->
+<!-- A dynamic table created via a nested PHP loop structure with random numbers. -->
+
+<?php 
+    $min = 1;
+    $max = 100;
+    $max_pairs = 10;
+?>
+
+<html>
+    <head>
+        <title>CSD-440: Server-Side Scripting</title>
+        
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald&display=swap">
+    </head>
+
+    <body>
+        <div class="content">
+            <h1>Module 2.2 Assignment</h1>
+            <hr/>
+
+            <div class="table-content">
+                <table>
+                    <tr>
+                        <th>Column 1</th>
+                        <th>Column 2</th>
+                    </tr>
+                    <tbody>
+                        <?php for ($i = 0; $i < $max_pairs; $i++): ?>
+                            <tr>
+                                <?php for ($x = 0; $x < 2; $x++):  ?>
+                                    <td><?php echo htmlspecialchars(random_int($min, $max)); ?></td>
+                                <?php endfor; ?>
+                            </tr>
+                        <?php endfor; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>    
+
+        <footer>
+            <p>Zac Baker | ©2026</p>
+        </footer>
+    </body>
+</html>
